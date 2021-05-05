1cadf9ad9be635b2c1b303253089bc49fd7c0c9c
Geoff <geoffrey.allman@googlemail.com>
2021-05-05 14:49:40 +0100

    [search] - converted static string to use state in the search form

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Search = () => {
-    const location = "Rugby, UK"
+    const [ location, setlocation ] = useState("Rugby, UK");
     return (
         <div className="">
             <form>
                 <label htmlFor="location">
                     Location
-                    <input id="" value={location} placeholder="Location"/>
+                    <input 
+                        id="" 
+                        value={location} 
+                        placeholder="Location" 
+                        onChange={ e => setlocation(e.target.value)}
+                    />
                 </label>
                 <button className="" type="submit">Submit</button>
             </form>
